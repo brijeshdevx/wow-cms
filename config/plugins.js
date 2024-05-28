c9df2f534596a285f9e89a5c9a274bfc18a1378b
samarthDevx <samarth.shinde@devxconsultancy.com>
2024-05-28 16:14:42 +0530

    Sets Media Library

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,30 @@
-module.exports = () => ({});
+
+  
+  module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          baseUrl:"https://media-uat.buywow.in",
+          rootPath: "public/wow-cms/",
+          s3Options: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+            region: env("AWS_REGION"),
+            params: {
+              ACL: env("AWS_ACL", "private"),
+              signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 6 * 24 * 60 * 60),
+              Bucket:  env("S3_BUCKET_NAME"),
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
