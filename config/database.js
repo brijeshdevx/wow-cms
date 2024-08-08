eb88ecd7613412119b650caeebcbdab53209c625
piyushjain <piyush@devxconsultancy.com>
2024-08-08 16:29:48 +0530

    merge jaimin

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'wow-dev-cms'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'DevxAdmin12#'),
+      password: env('DATABASE_PASSWORD', 'DevxAdmin'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
     },
     debug: false,
